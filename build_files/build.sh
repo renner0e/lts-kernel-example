f7515a6741da555d67afda4eb5a501ba6bd54007
renner <renner0@posteo.de>
2025-07-15 18:12:41 +0200

    feat: demonstrate LTS kernel

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,26 @@
 
 set -ouex pipefail
 
-### Install packages
+LTS=6.12
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+KERNEL=$(skopeo inspect --retry-times 3 docker://ghcr.io/ublue-os/akmods:longterm-${LTS}-"$(rpm -E %fedora)" | jq -r '.Labels["ostree.linux"]')
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:longterm-${LTS}-"$(rpm -E %fedora)"-${KERNEL} dir:/tmp/akmods
+AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+mv /tmp/rpms/* /tmp/akmods/
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y install /tmp/kernel-rpms/kernel-longterm{,-core,-modules,-modules-core,-modules-extra}-"${KERNEL}".rpm
 
-#### Example for enabling a System Unit File
+dnf5 -y remove kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-libs}
 
-systemctl enable podman.socket
+
+# Prevent kernel stuff from upgrading again
+dnf5 versionlock add kernel-longterm{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
+
+
+# Turns out we need an initramfs if we wan't to boot
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-longterm(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-longterm(|'"$KERNEL_SUFFIX"'-)//')"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
